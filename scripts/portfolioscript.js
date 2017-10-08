3540bef0400a91cc74bd9316b00e9a27949d09bb
Jon Lee <jonlee@nichols-ve501-0040.apn.wlan.upenn.edu>
2017-10-08 19:02:00 -0400

    updated design 7.1

---
+++ scripts/portfolioscript.js
@@ -0,0 +1,12 @@
+$(document).ready(
+function() 
+{
+    $('.anim').addClass('animated fadeInLeft');
+
+    var width = $(window).width();
+
+    if(width <= 800) {
+        $('.portfolio-gallery').hide();
+    }
+}); 
+
---
+++ scripts/script.js
@@ -0,0 +1,30 @@
+$(document).ready(
+function() 
+{
+    // var bgArray = ['hae.JPG', 'images/banner.png', 'images/gpupt/sample.png'];
+    // var bg = bgArray[Math.floor(Math.random() * bgArray.length)];
+
+    // $('body').css('background', bg);
+
+    // // If you have defined a path for the images
+    // // var path = 'images/bg/';
+
+    // // then you can put it right before the variable 'bg'
+    // $('body').css('background', bg);
+
+    // $('html').addClass('animated fadeInRight');
+
+    $('#jonlee').addClass('animated fadeInUp');
+    $('#eng').addClass('animated fadeInDown');
+
+    // $('#projectlink').click(
+    //     function(e)
+    //     {
+    //         e.preventDefault();
+    //         $('html').addClass('animated fadeOutDownBig');
+
+    //         window.location.href = "portfolio.html";
+    //         return false;
+    //     }
+    // );
+}); 
